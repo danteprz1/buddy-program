834f0fdb24af6f7ced0651bbe773cd7b20aaf34b
danteprz1 <dante.perez@wizeline.com>
2024-11-13 23:14:57 -0600

    Updating POs

--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,9 +1,20 @@
 from playwright.sync_api import Page
 
 class HomePage:
-    def __init__(self, page:Page):
-        self.page = Page
-        self.loginButton = page.get_by_role("link", name="Log in")
+    def __init__(self, page: Page):
+        self.page = page
+        self.login_button = page.get_by_role("link", name="Log in")
+        self.add_task_button = page.get_by_role("button", name="Add task")
+        self.task_name_input = page.get_by_placeholder("Task name")
+        self.submit_task_button = page.get_by_role("button", name="Add task")
 
-    def navigateToLoginPage(self):
-        self.page.click(self.loginButton)
+    def navigate_to_login_page(self):
+        self.login_button.click()
+
+    def create_task(self, task_name: str):
+        self.add_task_button.click()
+        self.task_name_input.fill(task_name)
+        self.submit_task_button.click()
+
+    def is_task_present(self, task_name: str):
+        return self.page.locator(f"text={task_name}").is_visible()
--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,17 +1,24 @@
 from playwright.sync_api import Page
 
-
 class LoginPage:
     def __init__(self, page: Page):
-        self.page = Page
-        self.emailField = page.get_by_label("Email")
-        self.passwordField = page.get_by_label("Password")
-        self.loginButton = page.get_by_role("button", name="Log in")
-        self.invalidLoginMessage = page.get_by_text("Wrong email or password")
+        self.page = page
+        self.email_field = page.get_by_label("Email")
+        self.password_field = page.get_by_label("Password")
+        self.login_button = page.get_by_role("button", name="Log in")
+        self.invalid_login_message = page.get_by_text("Wrong email or password")
 
-    def login(self, email, password):
-        self.page.fill(self.emailField, email)
-        self.page.fill(self.passwordField, password)
-        self.page.click(self.loginButton)
+    def login(self, email: str, password: str):
+        self.email_field.fill(email)
+        self.password_field.fill(password)
+        self.login_button.click()
+
+    def is_login_successful(self):
+        # Check if we successfully navigated to the home page or dashboard
+        return self.page.url == "https://todoist.com/app"
+
+    def is_login_unsuccessful(self):
+        # Check if an error message is displayed
+        return self.invalid_login_message.is_visible()
 
 
