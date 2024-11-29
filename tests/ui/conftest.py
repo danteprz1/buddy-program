0b61c01b380bc99a8540b897f8e05a3e8988fdda
danteprz1 <dante.perez@wizeline.com>
2024-11-29 10:50:56 -0600

    browserstack

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from playwright.sync_api import sync_playwright
 from config.config import BS_USERNAME, BS_ACCESS_KEY
+import json
+import urllib
+import subprocess
 
 
 @pytest.fixture(scope="session")
@@ -10,7 +13,20 @@ def playwright():
 
 @pytest.fixture(scope="session")
 def browser(playwright):
-    ws_endpoint = f"wss://cdp.browserstack.com/playwright?auth={BS_USERNAME}:{BS_ACCESS_KEY}"
+
+    desired_cap = {
+            'os': 'osx',
+            'os_version': 'big sur',
+            'browser': 'chrome',
+            'browser_version': 'latest',
+            'browserstack.username': {BS_USERNAME},
+            'browserstack.accessKey': {BS_ACCESS_KEY},
+            'project': 'Buddy',
+            'build': 'playwright-python-1',
+            'name': 'Test',
+            'resolution': '1280x1024'
+    }
+    ws_endpoint = "wss://cdp.browserstack.com/playwright?caps=" + urllib.parse.quote(json.dumps(desired_cap))
     browser = playwright.chromium.connect(ws_endpoint)
     yield browser
     browser.close()
