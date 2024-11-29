ccb3100449f3db539b5230a1f3cf3da249d21de0
danteprz1 <dante.perez@wizeline.com>
2024-11-29 12:42:17 -0600

    browserstack

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -17,7 +17,7 @@ def browser(playwright):
     desired_cap = {
             'browserstack.username': BS_USERNAME,
             'browserstack.accessKey': BS_ACCESS_KEY,
-            'resolution': '1600x900'
+            'resolution': '1600x1080'
     }
     ws_endpoint = "wss://cdp.browserstack.com/playwright?caps=" + urllib.parse.quote(json.dumps(desired_cap))
     browser = playwright.chromium.connect(ws_endpoint)
